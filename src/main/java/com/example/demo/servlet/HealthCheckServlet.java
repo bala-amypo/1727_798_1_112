3d3741f622f47f10e90db7609694c6573d28e8cc
bala-amypo <bala@amypo.in>
2025-12-25 09:51:39 +0000

    User commit

---
+++ src/main/java/com/example/demo/servlet/HealthCheckServlet.java
@@ -0,0 +1,17 @@
+package com.example.demo.servlet;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HealthCheckServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        response.setContentType("text/plain");
+        response.getWriter().write("Application is running");
+    }
+}
