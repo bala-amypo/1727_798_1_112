151941eefdc79013b132f2c9a3c93c9495537c54
bala-amypo <bala@amypo.in>
2025-12-27 05:17:40 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -20,19 +20,20 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
-public class SwaggerConfig {
+public class OpenApiConfig {
 
     @Bean
-    public OpenAPI digitalCredentialOpenAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Digital Credential Verification Engine")
-                        .description("API documentation for Academic Integrity Case Tracker")
-                        .version("1.0"));
-    }
+                
+                .servers(List.of(
+                        new Server().url("https://9232.pro604cr.amypo.ai/")
+                ));
+        }
 }
