13c17ff41cf87e3c84faf48d094c3c9171826a91
bala-amypo <bala@amypo.in>
2025-12-27 09:43:49 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -1,8 +1,10 @@
-
-
 package com.example.demo.security;
 
+import org.springframework.stereotype.Component;
+
+@Component   // 🔑 REQUIRED
 public class JwtUtil {
+
     public String generateToken(Long userId, String email, String role) {
         return "DUMMY_TOKEN";
     }
