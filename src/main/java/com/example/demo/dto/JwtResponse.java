5f3e0203117184860eb6917fd370ab90b223772e
bala-amypo <bala@amypo.in>
2025-12-28 10:18:20 +0000

    User commit

--- src/main/java/com/example/demo/dto/JwtResponse.java
+++ src/main/java/com/example/demo/dto/JwtResponse.java
@@ -9,10 +9,6 @@ public class JwtResponse {
 
     public JwtResponse() {}
 
-    public JwtResponse(String token) {
-        this.token = token;
-    }
-
     public JwtResponse(String token, Long id, String email, String role) {
         this.token = token;
         this.id = id;
@@ -20,15 +16,35 @@ public class JwtResponse {
         this.role = role;
     }
 
-    public String getToken() { return token; }
-    public void setToken(String token) { this.token = token; }
+    public String getToken() {
+        return token;
+    }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public void setToken(String token) {
+        this.token = token;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getRole() { return role; }
-    public void setRole(String role) { this.role = role; }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
